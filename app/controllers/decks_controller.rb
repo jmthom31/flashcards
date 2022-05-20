3aacc9c76bec251f5318d9581e663ee170200482
Julia Thomas <juliathomas@outlook.com>
2022-05-20 19:14:37 +0100

    updated decks controller

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,16 +1,30 @@
 class DecksController < ApplicationController
   def new
+    @deck = Deck.new
   end
 
   def create
+    @deck = Deck.new(deck_params)
+    if @deck.save
+      redirect_to decks_path, notice: 'Flashcard deck added'
+    else
+      render :new
+    end
   end
 
   def index
+    @decks = Deck.all.sort_by(&:subject)
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def deck_params
+    params.require(:deck).permit(:subject, :description)
   end
 end
